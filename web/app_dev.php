840553d9feb08c01d0c82d34eb97ab2022888c1e
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2018-05-21 21:58:58 +0200

    Docker integration (#1)
    * Add docker configuration
    
    * Set mysql version
    
    * Allow debug connections from docker host

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,15 +10,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+$dockerHostIp = exec("ip -4 route list match 0/0 | awk ' {print $3} '");
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'), true) || PHP_SAPI === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', $dockerHostIp), true) || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }
 
-require __DIR__.'/../app/autoload.php';
+require __DIR__ . '/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
