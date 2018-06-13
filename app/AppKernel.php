a63261198f88ce36fb17fd4ccf7f3e46303ef6f1
pfrydrych <pawel.frydrych@gammasoft.pl>
2018-06-13 15:37:29 +0200

    Revert "added hotdog controller"
    This reverts commit 79f255a

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,9 +21,6 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new MMProjectBundle\HotDogBundle\HotDogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/MMProjectBundle/HotDogBundle/HotDogBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MMProjectBundle\HotDogBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class HotDogBundle extends Bundle
-{
-}
